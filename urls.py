2620807d3f70b36169063a881fac400d6c71197e
skitomie <skitomie@gmail.com>
2024-08-18 19:03:48 +0100

    update

---
+++ urls.py
@@ -0,0 +1,8 @@
+# myproject/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # Include the relationship_app URLs
+]
