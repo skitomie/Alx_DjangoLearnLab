6018b4f37f3e1d1be5d5ae7f29d306e1257b7fbb
skitomie <skitomie@gmail.com>
2024-08-18 20:50:27 +0100

    update

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -29,7 +29,7 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
     
-class UserProfile(models.Model):
+
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
@@ -37,18 +37,10 @@ class UserProfile(models.Model):
     ]
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    Admin = models.CharField(max_length=10),
-    Librarian = models.CharField(max_length=10),
-    Member = models.CharField(max_length=10)
-  
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
     def __str__(self):
-        return f'{self.user.username} - {self.Admin}'
-    
-    def __str__(self):
-        return f'{self.user.username} - {self.Librarian}'
-    
-    def __str__(self):
-        return f'{self.user.username} - {self.Member}'
+        return f'{self.user.username} - {self.role}'
     
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
