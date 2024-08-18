eb232c47ac679a4e2c4dd164fe65057717fa57d1
skitomie <skitomie@gmail.com>
2024-08-18 19:08:14 +0100

    update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
 def list_books(request):
     book = Book.objects.all()
