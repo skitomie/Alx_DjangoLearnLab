d14656b3f6557942b468d97e3693dd21dffa9380
skitomie <skitomie@gmail.com>
2024-08-18 16:01:25 +0100

    update

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
