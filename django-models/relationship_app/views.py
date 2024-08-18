7a22ae190c9e2fd279eb80c85bad31475dc22401
skitomie <skitomie@gmail.com>
2024-08-18 16:23:17 +0100

    update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,14 +1,12 @@
+from django.views.generic.detail import DetailView
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
 
 def list_books(request):
     book = Book.objects.all()
     context = {'list_books': book}
 
     return render(request, 'relationship_app/list_books.html', context)
-
-from django.views.generic import DetailView
-from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
