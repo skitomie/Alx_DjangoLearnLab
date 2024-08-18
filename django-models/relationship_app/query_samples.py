d56a226837920fbf5fdfb11aecbdf0e51d458f81
skitomie <skitomie@gmail.com>
2024-08-18 13:29:41 +0100

    update

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -5,3 +5,8 @@ Library.objects.get(name=library_name)
 Library.objects.get(name="library_name")
 
 books.all()
+
+
+author_name = "author_name"
+Book.objects.filter(author_name)
+#Author.objects.get(name=author_name)
