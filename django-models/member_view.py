ef2d120d2115ba80a403b2f213938e3d8684a3b9
skitomie <skitomie@gmail.com>
2024-08-24 22:05:30 +0100

    outside

---
+++ django-models/member_view.py
@@ -0,0 +1,11 @@
+#from django.contrib.auth.models import User
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+from .models import Member
+
+@user_passes_test(Member)
+def Member(request):
+    return render(request, 'Member.html')
+
+#user = User.objects.create_user(username='john', password='password')
+#user_profile = UserProfile.objects.create(user=user, role='Member')
