8edc0cf6f61d805a7d91e298f32afe167f378c36
skitomie <skitomie@gmail.com>
2024-08-25 04:15:48 +0100

    Update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import SomeModel
 
-@permission_required('bookshelf.can_view', raise_exception=True)
+@permission_required('bool_list.can_view', raise_exception=True)
 def view_somemodel(request, pk):
     obj = get_object_or_404(SomeModel, pk=pk)
     return render(request, 'bookshelf/somemodel_detail.html', {'object': obj})
 
-@permission_required('bookshelf.can_edit', raise_exception=True)
+@permission_required('book_list.can_edit', raise_exception=True)
 def edit_somemodel(request, pk):
     obj = get_object_or_404(SomeModel, pk=pk)
     if request.method == 'POST':
