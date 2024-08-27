5a1e452878b2fde24511e2b04980d450b7cc8a7c
skitomie <skitomie@gmail.com>
2024-08-27 09:48:35 +0100

    update

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from .views import BookList
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('api/', include("api.urls")),
 ]
