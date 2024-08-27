4f6fe03c028c6b8a548aab9b4847da1ca51dbeef
skitomie <skitomie@gmail.com>
2024-08-27 10:40:29 +0100

    update

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import BookList
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 # Create a router and register the ViewSet
 router = DefaultRouter()
@@ -12,4 +13,5 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics.ListAPIView
+#from rest_framework.generics.ListAPIView
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminOrReadOnly
 
 class BookViewSet(viewsets.ModelViewSet):
     # A ViewSet for viewing and editing book instances.
@@ -11,5 +13,14 @@ class BookViewSet(viewsets.ModelViewSet):
 
 
 class BookList(ListAPIView):
+
+  """
+    A ViewSet for viewing and editing book instances.
+    Requires authentication for all actions.
+    Only admin users can modify data; others can only view it.
+"""
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access this view
+    permission_classes = [IsAdminOrReadOnly]
