8de8cb1421a18b3683d89546a135ae79acd31a0b
skitomie <skitomie@gmail.com>
2024-09-15 17:35:41 +0100

    django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('post/new/', views.CreateView.as_view(), name='post-create'),
     path("post/<int:pk>/update/", views.UpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete/', views.DeleteView.as_view(), name='post-delete'),
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 ]
